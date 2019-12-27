f093fb95d886166ac0c25f1d02c544997ac75267
Mo Dongsong <modongsong@126.com>
2019-12-27 15:15:09 +0800

    Updated:

--- components/nvidia/install-cuda.sh
+++ components/nvidia/install-cuda.sh
@@ -7,5 +7,8 @@
 #sudo apt-get update
 #sudo apt-get -y --allow-unauthenticated install cuda
 
+https://developer.download.nvidia.cn/compute/cuda/10.0/secure/Prod/local_installers/cuda_10.0.130_410.48_linux.run?6MLjtqYt2Bov6gnjnHi_nxnm6RqJILshutUHtxpWMpy1HJVsZh5FmSAlrwofebPUkSg8KgUzjVbwSetagqY4RoTkab4vVMleI0QfFCKpoMQ1KKApAv7hochkoZ15FE0NYhEtQ8edtqHdio_urAewTv6r142oVn7Ib2jn6_pllByAPl54TYvDR5Q1TCc
+
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 sudo sh cuda_10.2.89_440.33.01_linux.run
+

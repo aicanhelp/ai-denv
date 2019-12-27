69c2a9cd340fcebcffbe1562a00c2478751a6deb
Mo Dongsong <modongsong@126.com>
2019-12-27 15:46:50 +0800

    Updated:

--- components/nvidia/install-cuda.sh
+++ components/nvidia/install-cuda.sh
@@ -9,6 +9,8 @@
 
 https://developer.download.nvidia.cn/compute/cuda/10.0/secure/Prod/local_installers/cuda_10.0.130_410.48_linux.run?6MLjtqYt2Bov6gnjnHi_nxnm6RqJILshutUHtxpWMpy1HJVsZh5FmSAlrwofebPUkSg8KgUzjVbwSetagqY4RoTkab4vVMleI0QfFCKpoMQ1KKApAv7hochkoZ15FE0NYhEtQ8edtqHdio_urAewTv6r142oVn7Ib2jn6_pllByAPl54TYvDR5Q1TCc
 
+https://developer.download.nvidia.cn/compute/machine-learning/cudnn/secure/7.6.5.32/Production/10.0_20191031/Ubuntu18_04-x64/libcudnn7-dev_7.6.5.32-1%2Bcuda10.0_amd64.deb?K_wE5IFxo9gGYXyrt2RiNVu7rBYm-lViEp69GavMvUQj8nptyW3_IYEaTiSCmS1KVxZiZT-JjK52zhcwrWSaCApSDp6LmvHN1MyYOmBwTHbWffBzkgQa9ppD9lYStm4NIug9hdsD1oQPo61qmEBvPUTndrL90lckkUAXxZ9L4djKzDCE5drHhfT3Ro1feZfoSdxANGhMAZRafL8B2YfxxLK-scZzgR3mWA2auuRagtZWbVCkt4l11Ph58X-N8ygITeRDVg
+
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 sudo sh cuda_10.2.89_440.33.01_linux.run
 

9b07a27c231b4d9510fcbc2848dd0dd1974ce490
Mo Dongsong <modongsong@126.com>
2019-12-31 14:51:27 +0800

    Updated:

---
+++ components/rancher/deploy-rancher-single.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source config.default
+
+curDir=`pwd`
+baseDir=$(dirname $0)
+cd $baseDir
+home=`pwd`
+
+docker_cmd="sudo docker"
+
+$docker_cmd stop rancher
+$docker_cmd rm rancher
+
+$docker_cmd run -d --name rancher --restart=unless-stopped \
+  -p 8089:80 -p 8443:443 \
+  -v $home/rancher:/var/lib/rancher \
+  rancher/rancher:latest
+
+cd $curDir

23cf878ea4e33ff1498db60cfa0d863202824c36
Mo Dongsong <modongsong@126.com>
2020-01-01 18:08:18 +0800

    Updated:

--- components/rancher/deploy-rancher-single.sh
+++ components/rancher/deploy-rancher-single.sh
@@ -13,7 +13,7 @@ $docker_cmd stop rancher
 $docker_cmd rm rancher
 
 $docker_cmd run -d --name rancher --restart=unless-stopped \
-  -p 8089:80 -p 8443:443 \
+  -p 8001:80 -p 8002:443 \
   -v $home/rancher:/var/lib/rancher \
   rancher/rancher:latest
 

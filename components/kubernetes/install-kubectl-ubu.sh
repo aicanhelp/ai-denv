b302a9d71f8df7827ccdfddf32f1aff84adbb5a5
Mo Dongsong <modongsong@126.com>
2020-01-02 15:08:25 +0800

    Updated:

--- components/kubernetes/install-kubectl-ubu.sh
+++ components/kubernetes/install-kubectl-ubu.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update && apt-get install -y apt-transport-https
 sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl

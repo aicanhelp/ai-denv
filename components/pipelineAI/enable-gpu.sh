e766835a3a468ff443393f78b4054235e6569f89
Mo Dongsong <modongsong@126.com>
2020-01-03 12:51:03 +0800

    Updated:

--- components/pipelineAI/enable-gpu.sh
+++ components/pipelineAI/enable-gpu.sh
@@ -4,4 +4,6 @@
 
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v1.11/nvidia-device-plugin.yml
 
+kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta4/nvidia-device-plugin.yml
+
 kubectl get nodes "-o=custom-columns=NAME:.metadata.name,MEMORY:.status.allocatable.memory,CPU:.status.allocatable.cpu,GPU:.status.allocatable.nvidia\.com/gpu"

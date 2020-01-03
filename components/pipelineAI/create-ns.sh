3bffd40fc31721cf28499de7ca6fcc5f9ca5014e
Mo Dongsong <modongsong@126.com>
2020-01-03 11:40:30 +0800

    Updated:

--- components/pipelineAI/create-ns.sh
+++ components/pipelineAI/create-ns.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+#specify cluster: --kubeconfig /custom/path/kube.config
+
 kubectl create namespace kubeflow
 
 kubectl config set-context $(kubectl config current-context) --namespace=kubeflow
--- components/pipelineAI/enable-gpu.sh
+++ components/pipelineAI/enable-gpu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#specify cluster: --kubeconfig /custom/path/kube.config
+
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v1.11/nvidia-device-plugin.yml
 
 kubectl get nodes "-o=custom-columns=NAME:.metadata.name,MEMORY:.status.allocatable.memory,CPU:.status.allocatable.cpu,GPU:.status.allocatable.nvidia\.com/gpu"

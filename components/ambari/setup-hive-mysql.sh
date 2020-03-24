c5cd5226bb8404f0ab8cc16c903e18ef32527b8e
Mo Dongsong <modongsong@126.com>
2020-03-24 13:00:26 +0800

    Updated:

---
+++ components/ambari/setup-hive-mysql.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo mysql -u root -pbeijing -e "
+create database hive;
+CREATE USER 'hive'@'%' IDENTIFIED BY 'hive';
+GRANT ALL PRIVILEGES ON *.* TO 'hive'@ '%' IDENTIFIED BY 'hive' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'hive'@'localhost' IDENTIFIED BY 'hive' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+"
